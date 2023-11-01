d0699304e30a16eb935788edf90a95fee50d0e29
FathyaAzra <fathyaazra@gmail.com>
2023-11-01 17:11:49 +0700

    fitur ganti tema1

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-const tomboltema= document.querySelectorAll('[type="tema"]')
+const tomboltema= document.querySelectorAll('.btn[type="tema"]')
 const divnav= document.querySelectorAll(".navbar")
 const logo= document.querySelectorAll(".logo a")
 const category=document.querySelector(".category")
+
 const divIsi= document.querySelector(".isi")
-const JudulKategori= document.querySelectorAll(".JudulKategori")
+const head= document.getElementsByClassName("headline")[0]
+const head1= document.getElementsByClassName("headline")[1]
 
-const pilihan= document.querySelector(".cardbody a")
-
-
+const JudulK= document.querySelectorAll(".JudulKategori")
 
 let judulnews=""
 let isiNews = ""
 let tema=0  //tema pink
 
 function GantiTema(){
-if (tema===0){//tema pink
-divIsi.style.backgroundColor= "rgb(20, 20, 20)"
-tema=1
-}
-else{ //tema dark
-divIsi.style.backgroundColor= "rgb(255, 225, 225)"
-tema=0
-}
+    if (tema===0){//tema pink
+        tema=1
+        divIsi.style.backgroundColor= "rgb(40, 40, 40)";
+        head.style.color="rgb(255, 225, 225)";
+        head1.style.color="rgb(255, 225, 225)";
+    }
+    else{ //tema dark
+        tema=0;
+        divIsi.style.backgroundColor= "rgb(255, 225, 225)";
+        head.style.color="rgb(40, 40, 40)";
+        head1.style.color="rgb(40, 40, 40)";
+    }
 }
 
-for (let i = 0; i < tomboltema.length; i++) {
-tomboltema[i].addEventListener("click", function()
+for (let i = 0; i < tomboltema.length; i++) 
 {
-GantiTema();
-})
+    tomboltema[i].addEventListener
+    ("click", function()
+    {
+        console.log("Hello");
+        GantiTema();
+    }
+    )
 }
